609407285d3f9a926b32b705f0a4cada576bdaeb
SylvainRousset <sylvaindebisca@hotmail.fr>
2025-03-18 20:10:19 +0100

    Update page.tsx

--- src/app/produit/[id]/page.tsx
+++ src/app/produit/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from 'react';
 import ProductDetailClient from './ProductDetailClient';
 
-type Props = {
-  params: { id: string }
+type Params = {
+  id: string
 }
 
-export default function ProductPage(props: Props) {
+export default function ProductPage(props: { params: Params }) {
   return (
     <Suspense fallback={<div>Chargement...</div>}>
       <ProductDetailClient params={props.params} />
