674a0e3d2cc7e8954f0f306adab5f242237aba98
SylvainRousset <sylvaindebisca@hotmail.fr>
2025-03-27 19:28:31 +0100

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,32 @@
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <Link href={`/produit/${product.id}`}>{product.name}</Link>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.description}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">{Number(product.price).toFixed(2)} €</p>
-              </div> 
+import Image from 'next/image';
+import Link from 'next/link';
+import { Product } from '@/lib/firebaseAdmin';
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export default function ProductCard({ product }: ProductCardProps) {
+  return (
+    <div className="group relative">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={500}
+          height={500}
+          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <Link href={`/produit/${product.id}`}>{product.name}</Link>
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{product.description}</p>
+        </div>
+        <p className="text-sm font-medium text-gray-900">{Number(product.price).toFixed(2)} €</p>
+      </div>
+    </div>
+  );
+} 
